bc072d9e4cd6072f1aebf49c956f4a192d038149
bremp <byadao@gmail.com>
2020-08-15 18:03:21 -1000

    Final implementation.

--- src/main/java/com/bremp/utils/ListUtils.java
+++ src/main/java/com/bremp/utils/ListUtils.java
@@ -9,23 +9,21 @@ public class ListUtils {
   }
 
   /**
+   * Splits a list into a given number of sublists. Tries to partition the list in equal parts. The
+   * list content is distributed in order to the given sublists.
    * https://stackoverflow.com/questions/16616560/is-there-a-simple-way-to-split-one-list-into-x-sublists
-   * @param list
-   * @param numSubLists
-   * @param <T>
-   * @return
+   *
+   * @param list        the list to split.
+   * @param numSubLists the number of sublists.
+   * @return A list of sublists.
    */
-  public static  <T> List<List<T>> orderedSplit(List<T> list, int numSubLists) {
+  public static <T> List<List<T>> splitToSubLists(List<T> list, int numSubLists) {
     if (list == null) {
       throw new NullPointerException("List is null.");
     }
 
     if (numSubLists <= 0) {
       throw new IllegalArgumentException("The list must be divided by an amount greater than 0.");
-    }
-
-    if (numSubLists > list.size()) {
-      throw new IllegalArgumentException("The size of the list is not sufficient for that distribution.");
     }
 
     List<List<T>> result = new ArrayList<>(numSubLists);
--- src/test/java/com/bremp/utils/ListUtilsTest.java
+++ src/test/java/com/bremp/utils/ListUtilsTest.java
@@ -1,25 +1,76 @@
 package com.bremp.utils;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-public class ListUtilsTest {
+@DisplayName("ListUtils - splitToSubLists")
+class ListUtilsTest {
 
-  // TODO: Test guards and use params for supplying varying lists.
   @Test
-  void partition() {
-    List<String> source = Arrays.asList("1", "2", "3", "4", "5");
-    int buckets = 4;
+  @DisplayName("throws exception when list is null.")
+  void whenListIsNull() {
+    Assertions.assertThrows(NullPointerException.class, () -> {
+      List<String> list = null;
+      int numOfLists = 4;
+      ListUtils.splitToSubLists(list, numOfLists);
+    });
+  }
 
-    List<List<String>> actual = ListUtils.orderedSplit(source, buckets);
+  @Test
+  @DisplayName("throws exception when number of sublists is zero.")
+  void whenNumberOfSubListsIsZero() {
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {
+      List<String> list = new ArrayList<>();
+      int numOfLists = 0;
+      ListUtils.splitToSubLists(list, numOfLists);
+    });
+  }
+
+  @Test
+  @DisplayName("when number of sublists is greater than list size.")
+  void whenNumberOfSubListsIsGreaterThanListSize() {
+    List<String> list = new ArrayList<>();
+    int numOfLists = 1;
+
+    List<List<String>> actual = ListUtils.splitToSubLists(list, numOfLists);
+
+    assertThat(actual.size(), is(1));
+    assertThat(actual.get(0), is(empty()));
+  }
+
+  @ParameterizedTest
+  @DisplayName("with varying list sizes.")
+  @CsvSource({
+      "1, 1",
+      "'1,2', 1",
+      "'1,2,3', 1",
+      "'1,2,3,4', 2",
+      "'1,2,3,4,5', 2",
+      "'1,2,3,4,5,6', 2",
+      "'1,2,3,4,5,6,7', 3",
+      "'1,2,3,4,5,6,7,8', 3",
+      "'1,2,3,4,5,6,7,8,9', 3"
+  })
+  void withVaryingListSizes(String listContent, int expectedSubListSize) {
+    List<String> list = Arrays.asList(listContent.split(","));
+    int numOfLists = 3;
+
+    List<List<String>> actual = ListUtils.splitToSubLists(list, numOfLists);
 
     actual.forEach(s -> System.out.println(s.toString()));
 
-    assertThat(actual.size(), is(4));
+    assertThat(actual.size(), is(3));
+    assertThat(actual.get(0).size(), is(expectedSubListSize));
   }
 
 }
